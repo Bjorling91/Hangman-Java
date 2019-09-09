964095b990d5fe4d1445a9d9ff8298b662982c38
Fredrik Björling <bjorling91@gmail.com>
2019-09-09 12:39:14 +0200

    Labb 1 initierat

--- Labb1/app/src/main/java/fredbjor101/kau/se/myapplication/MainActivity.java
+++ Labb1/app/src/main/java/fredbjor101/kau/se/myapplication/MainActivity.java
@@ -1,13 +1,27 @@
 package fredbjor101.kau.se.myapplication;
 
+import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
+
+    protected Button myButton;
+    String myString = "Det här är en toast";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        myButton = (Button)findViewById(R.id.buttonId);
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View V);
+        });
+
     }
 }
