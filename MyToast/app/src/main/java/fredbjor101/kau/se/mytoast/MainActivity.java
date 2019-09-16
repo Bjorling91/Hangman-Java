2a397430a811c547793e58c427faa7d34ccf5269
Fredrik Björling <bjorling91@gmail.com>
2019-09-16 18:47:52 +0200

    Labb 1 initierat

--- MyToast/app/src/main/java/fredbjor101/kau/se/mytoast/MainActivity.java
+++ MyToast/app/src/main/java/fredbjor101/kau/se/mytoast/MainActivity.java
@@ -13,28 +13,28 @@ import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
-    private Button button;
+    private Button myButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = (Button) findViewById(R.id.button);
-        button.setOnClickListener(new View.OnClickListener() {
+        myButton = (Button) findViewById(R.id.button);
+        myButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 showToast();
             }
 
             public void showToast() {
-                LayoutInflater inflater = getLayoutInflater();
-                View layout = inflater.inflate(R.layout.toast_layout, (ViewGroup) findViewById(R.id.toast_root));
+                LayoutInflater myInflater = getLayoutInflater();
+                View myLayout = myInflater.inflate(R.layout.toast_layout, (ViewGroup) findViewById(R.id.toast_root));
 
                 Toast toast = new Toast(getApplicationContext());
                 toast.setGravity(Gravity.CENTER,0, 0);
                 toast.setDuration(Toast.LENGTH_LONG);
-                toast.setView(layout);
+                toast.setView(myLayout);
 
                 toast.show();
             }
